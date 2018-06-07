871a6d6d60c09d224c0b8c08420bb35748378aa5
Joseph Senders <jasenders@gmail.com>
2018-06-07 13:52:54 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,21 +3,26 @@ array.include?(element)
 end
 
 def using_sort(array)
-
+array = ["lil' bub", "grumpy cat", "maru"]
+array.sort
 end
 
 def using_reverse(array)
-
+array = ["lil' bub", "grumpy cat", "maru"]
+array.reverse
 end
 
 def using_first(array)
-
+array = ["lil' bub", "grumpy cat", "maru"]
+array.first
 end
 
 def using_last(array)
-
+array = ["lil' bub", "grumpy cat", "maru"]
+array.last
 end
 
 def using_size(array)
-
+array = ["lil' bub", "grumpy cat", "maru"]
+array.size
 end
